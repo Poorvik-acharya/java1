55d431f4c005c133c89633363e7b295eb4a89ffb
Poorvik-acharya <poorvik.23cs092@sode-edu.in>
2024-12-17 22:07:47 +0530

    Update and rename P1.java to T1.java

--- T1.java
+++ T1.java
@@ -1,19 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author SMV
- */
 import java.util.Scanner;
-public class P1 {
-
-    /**
-     * @param args the command line arguments
-     */
+public class T1 {
     public static void main(String[] args) {
         // TODO code application logic here
         int n1=Integer.parseInt(args[0]);

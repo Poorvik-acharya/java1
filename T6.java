f4d180354c206291be41e9da365e8204e12f8505
Poorvik-acharya <poorvik.23cs092@sode-edu.in>
2024-12-17 22:01:57 +0530

    Update T6.java

--- T6.java
+++ T6.java
@@ -26,7 +26,7 @@ class Triangle1 extends Shape1{
     double calculatePerimeter() {
         return side1+side2+side3;}}
 
-public class TestAbstractShape {
+public class T6 {
     public static void main(String[] args) {
         Circle1 circle1=new Circle1(5);
         System.out.println("Area and Circumference of Circle are");
